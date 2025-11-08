c84eb1ab5ddf6e64aeca8a369e3664b7d4b40d19
kge <kge@bitwormhole.com>
2025-11-08 09:59:40 +0800

    up to 'lang/v0.9.1'

--- lang/module_t.go
+++ lang/module_t.go
@@ -2,8 +2,8 @@ package lang
 
 const (
 	theModuleName     = "github.com/starter-go/v1/lang"
-	theModuleVersion  = "v0.0.0"
-	theModuleRevision = 0
+	theModuleVersion  = "v0.9.1"
+	theModuleRevision = 1
 )
 
 func GetThisModule() Module {
